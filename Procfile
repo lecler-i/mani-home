f229910f19df53d274d821d349363b2190dfd6a7
Thomas LECLERCQ <degnus@gmail.com>
2017-05-14 18:14:50 +0530

    Add heroku config

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: MIX_ENV=prod-heroku mix phoenix.server
---
+++ config/prod-heroku.exs
@@ -0,0 +1,18 @@
+use Mix.Config
+
+config :manihome, Manihome.Endpoint,
+  url: [scheme: "https", host: "https://mani-home-prod.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],  http: [port: {:system, "PORT"}],
+  cache_static_manifest: "priv/static/manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+config :logger, level: :info
+
+# Configure your database
+config :manihome, Manihome.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
+import_config "prod.secret.exs"
