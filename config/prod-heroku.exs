de02bd3769856ff0c15d4bde3a75a9d287ad82aa
Thomas LECLERCQ <degnus@gmail.com>
2017-05-14 18:58:10 +0530

    Fix syntax error in prod-heroku config file

--- config/prod-heroku.exs
+++ config/prod-heroku.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :manihome, Manihome.Endpoint,
   url: [scheme: "https", host: "https://mani-home-prod.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],  http: [port: {:system, "PORT"}],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :logger, level: :info
